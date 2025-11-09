c09d3b1ec482e4631cac11457dad2d7c7edd6337
Jacob Hoang <145881019+hoangjacob@users.noreply.github.com>
2025-11-09 17:12:41 -0600

    Initial Commit

---
+++ worker.js
@@ -0,0 +1,17 @@
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  if (url.hostname in ORIGINS) {
+  const target = ORIGINS[url.hostname]
+  url.hostname = target
+  return fetch(url.toString(), request)
+  }
+  return fetch(request)
+  }
+  addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+  })
+   
+  const ORIGINS = {
+  'cubari.jacob-hoang2010.workers.dev': 'cubari.moe'
+  }   
+  
